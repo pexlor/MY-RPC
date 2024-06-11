e102ae226f47bb38c387733d3c7a669793a62725
pexlor <1018734423@qq.com>
2024-06-11 19:59:03 +0800

    add log

--- rocket/common/log.h
+++ rocket/common/log.h
@@ -3,14 +3,65 @@
 #include <string>
 #include <queue>
 #include <memory>
+#include <sstream>
+
 
 namespace rocket {
-    
+
+template<typename... Args>
+
+std::string formatString(const char* str,Args&&... agrs)
+{
+    int size = snprintf(nullptr,0,str,args...);
+
+    std::string result;
+    if(size > 0)
+    {
+        result.resize(size);
+        snprintf(&result[0],size+1,str,args...);
+    }
+    return result;
+}
+
+
+#define DEBUGLOG(str, ... ) \
+    std::string msg = (new rocket::LogEvent(rocket::LogLevel::Debug))->toSting() + rocket::formatString(str,##__VA_ARGS__);\
+    rocket::Logger::GetGloballLogger()->pushlog(msg); \
+    rocket::Logger::GetGloballLogger()->log(); \
+
 enum LogLevel {
     Unknown = 0,
     Debug = 1,
     Info = 2,
     Error = 3
+};
+
+
+class Logger {
+public:
+    typedef std::shared_ptr<Logger> s_ptr;
+    void pushlog(const std::string & msg);
+    static Logger * GetGloballLogger();
+    void log();
+private:
+    LogLevel m_set_level;
+    std::queue<std::string> m_buffer;
+};
+
+std::string LogLevelToString(LogLevel level);
+
+class LogEvent {
+public:
+    LogEvent(LogLevel level);
+    std::string getFileName();
+    int32_t getLogLevel();
+    std::string  toSting();
+private:
+    std::string m_file_name;
+    int32_t m_file_line;
+    int32_t m_pid;
+    int32_t m_thread_id;
+    LogLevel m_level;
 };
 
 
