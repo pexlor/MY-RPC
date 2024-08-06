2252d56c7666b4de564432355d14ddf22e149817
pexlor <1018734423@qq.com>
2024-08-06 18:52:27 +0800

    add somthing code

--- rocket/common/asynclog.h
+++ rocket/common/asynclog.h
@@ -1,2 +1,15 @@
 #pragma once
+#include <string>
+#include <mutex>
+class AsyncLog {
+private:
+    std::mutex bufMutex;
+    Buffer currentBuf;
+    
 
+
+public:
+    void append(const std::string * logline,int len);
+
+
+};
